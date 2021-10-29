40c2c085f42e67a822b69121a5261374b6de306a
Rahul-K-A <karahul209@gmail.com>
2021-10-29 13:30:38 +0530

    Formatted code

--- NodeMCUESP8266_Motion/NodeMCUESP8266_Motion.ino
+++ NodeMCUESP8266_Motion/NodeMCUESP8266_Motion.ino
@@ -15,42 +15,10 @@ const char *Password = "YourWifiPasswordHere";  // Enter WiFi password
 
 // MQTT Broker
 const char *Mqtt_Broker = "Your broker IP Adress here";
-const char *Mqtt_Username = "Your MQTT Server username here (not mandatory if you are just publishing)";
-const char *Mqtt_Password = "Your MQTT Server password here (not mandatory if you are just publishing) ";
+const char *Mqtt_Username = "Your MQTT Server username here";
+const char *Mqtt_Password = "Your MQTT Server password here";
 const char *Topic = "HomeSecurity/Motion";
 const int Mqtt_Port = 1883;
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-
-void setup() {
-   Serial.begin(9600); // Starting Serial Terminal
-   //Connect to Wifi
-    WiFi.begin(ssid, Password);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.println("Connecting to WiFi..");
-    }
-    Serial.println("Done");
-   
-    
-    //Connect to Broker
-    client.setServer(Mqtt_Broker, Mqtt_Port);
-    while (!client.connected())
-    {
-      String client_id = "esp8266-client-";
-      client_id += String(WiFi.macAddress());
-      Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
-      if (client.connect(client_id.c_str(), Mqtt_Username, Mqtt_Password)) {} 
-      else {
-          Serial.print("failed with state ");
-          Serial.print(client.state());
-          delay(2000);
-      }
-    }
-}
 
 void loop() {
    long duration, cm;
@@ -68,22 +36,12 @@ void loop() {
    //Get distance to object in centi meters
    cm = GetDistanceCm(duration);
    Serial.println(cm);
-   if(cm<=50.0f)
-   {
-     Serial.println("MOTION DETECTED");
-     client.publish(Topic, "MOTION DETECTED");
-     //delay for 10 seconds to allow camera to finish capturing video
-     delay(10000);
-   }
-   else{
-    Serial.println("NO MOTION DETECTED");
-    client.publish(Topic, "NO MOTION DETECTED");
-    }
    delay(100);
 }
 
 
 
 long GetDistanceCm(long microseconds) {
-   return microseconds / (29*2);
+  //Eqn to convert microseconds to cms
+   return microseconds / 58;
 }
